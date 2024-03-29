7c459597794e853f8d822b250deca1116d06cb85
harmeesh <147924282+harmeeshsingh@users.noreply.github.com>
2024-03-29 14:56:21 -0500

    CM1 for madlib

---
+++ madlib.py
@@ -0,0 +1,5 @@
+#this program is use to play a with some random grammar in so complete the sentece. It also includes the input from the user.
+name= input(str('enter your name: '))
+hero = input(str("Your faverot superchero is: "))
+age = int(input("age of your hero is:"))
+print(f"Hello! how are you?\n{name}\nIn this game we will write about a comic super hero. Let me guss the name of your super hero is {hero} and he is {age} years old. {hero} can live on both water and land but can't fly. ")
